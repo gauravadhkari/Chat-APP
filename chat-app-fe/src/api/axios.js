d33831180f218500c487a60df283c9e7c306c9e3
gauravadhikari <adhikarigaurav05@gmail.com>
2026-08-26 14:36:23 +0530

    api

--- chat-app-fe/src/api/axios.js
+++ chat-app-fe/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+export const API_URL = import.meta.env.VITE_API_URL || "https://chat-app-2-98p2.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
--- chat-app-fe/src/services/api.js
+++ chat-app-fe/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from  "axios"
 
  const api = axios.create({
-  baseURL:"http://localhost:5000/api",
+  baseURL:"https://chat-app-2-98p2.onrender.com/api",
  });
 const API_URL = import.meta.env.VITE_API_URL;
 
